a17617753ada050ed6c671ec567d74bd593071f1
Roman Galeev <jamhed@2600hz.com>
2016-05-19 19:09:02 +0200

    monster-ui binding

--- monster-ui/build/run.sh
+++ monster-ui/build/run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
-sed -i "/api.zswitch.net/s/api.zswitch.net/$MY_IP/" /var/www/html/monster-ui/js/config.js
-sed -i "/api.zswitch.net/s/https:\/\/api.zswitch.net:8443/http:\/\/$MY_IP:8000/" /var/www/html/kazoo-ui/config/config.js
+cp etc/config-kazoo.js /var/www/html/kazoo-ui/config/config.js
+cp etc/config-monster-ui.js /var/www/html/monster-ui/config/config.js
 
-echo monster-ui ip $MY_IP
 nginx -g "daemon off;"
