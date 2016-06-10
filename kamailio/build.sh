fa40ce8e88e0132c517e5fbe57be120e68a8ed1c
Roman Galeev <roman@jamhed.tk>
2016-06-10 14:27:06 +0200

    temporary version fix for libevbuffsock

--- kamailio/build.sh
+++ kamailio/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t kazoo/kamailio .
+docker build $1 -t kazoo/kamailio .
--- kamailio/build/setup.sh
+++ kamailio/build/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-git clone --depth 1 --no-single-branch https://github.com/mreiferson/libevbuffsock libevbuffsock
+git clone https://github.com/mreiferson/libevbuffsock libevbuffsock
 git clone --depth 1 --no-single-branch https://github.com/nsqio/libnsq libnsq
 git clone --depth 1 --no-single-branch https://github.com/kamailio/kamailio.git kamailio
+
+# temporary
+cd libevbuffsock
+git checkout ee239ec
