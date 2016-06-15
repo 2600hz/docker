20b89d4a63e5b2b0f10911422ef9891802895c14
Roman Galeev <roman@jamhed.tk>
2016-06-15 11:15:39 +0200

    monster-ui build script improvement

--- monster-ui/build/setup.sh
+++ monster-ui/build/setup.sh
@@ -2,13 +2,12 @@
 rm -rf $WWW/html
 mkdir -p $WWW/html
 cd $WWW
-git clone $REPO html
+git clone --depth 1 --no-single-branch $REPO html
 
 echo "Setup public apps"
 cd $WWW/html/apps
-git clone https://github.com/2600hz/monster-ui-accounts accounts
-git clone https://github.com/2600hz/monster-ui-callflows callflows
-git clone https://github.com/2600hz/monster-ui-numbers numbers
-git clone https://github.com/2600hz/monster-ui-pbxs pbxs
-git clone https://github.com/2600hz/monster-ui-voip voip
-git clone https://github.com/2600hz/monster-ui-webhooks webhooks
+
+for app in accounts callflows numbers pbxs voip webhooks
+do
+	git clone --depth 1 --no-single-branch https://github.com/2600hz/monster-ui-$app $app
+done
