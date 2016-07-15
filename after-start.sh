222ea416e856686a4eb459dc8cc2dbc28606bbf2
Roman Galeev <roman@jamhed.tk>
2016-07-15 21:01:56 +0200

    reload dispatcher

--- after-start.sh
+++ after-start.sh
@@ -11,3 +11,5 @@ docker cp apps kazoo.kazoo:/home/user
 rm -rf apps
 
 ./sup crossbar_maintenance init_apps /home/user/apps http://kazoo.kazoo:8000/v2
+
+docker exec -ti kamailio.kazoo kamctl dispatcher reload 
