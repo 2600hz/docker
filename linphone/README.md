7b9c4065c5e85e462ea9616b822bd57927b1ef86
Roman Galeev <roman@jamhed.tk>
2016-07-16 02:24:24 +0200

    Linphone clients for kazoo

--- linphone/README.md
+++ linphone/README.md
@@ -1,8 +1,15 @@
+Kazoo Linphone
+==============
+
+To run multiple sip clients for testing purposes.
+
 Usage
 =====
 
 ```sh
 ./build.sh
-./run.sh 1
-./cmd.sh 1 register --host kamailio.kazoo --username $USERNAME --password $PASSWORD
+./run.sh $ID $TARGET
+./register.sj $ID $TARGET $SIP_NAME $SIP_PASSWORD
 ```
+
+ID is the container/linphone instance number, TARGET is the alias of kamailio host.
--- linphone/run.sh
+++ linphone/run.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 ID=$1
+TARGET=$2
 : ${ID:=1}
+: ${TARGET:=company.kazoo}
+KAMAILIO_IP=`../bin/get-ip kamailio.kazoo`
+echo Linphone $ID for $TARGET as $KAMAILIO_IP
 NAME=linphone$ID.kazoo
 docker stop $NAME
 docker rm $NAME
-docker run -td --net kazoo -h $NAME --name $NAME kazoo/linphone
+docker run -td $ARGS \
+	--add-host=$TARGET:$KAMAILIO_IP \
+	--net kazoo \
+	-h $NAME --name $NAME \
+	-v /dev/snd:/dev/snd \
+	--privileged \
+	kazoo/linphone
