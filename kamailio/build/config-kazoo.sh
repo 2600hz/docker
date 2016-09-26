5b24148ffded8da60d05d19b5cacf64249c9f609
Roman Galeev <jamhed@2600hz.com>
2016-09-26 17:34:57 +0200

    update to new kazoo configs

--- kamailio/build/config-kazoo.sh
+++ kamailio/build/config-kazoo.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-cp -a etc/kamailio /etc/
+mkdir /etc/kazoo
+mkdir /etc/kamailio
+cp -a etc/kamailio /etc/kazoo
+cp etc/kamailio/kamctlrc /etc/kamailio/
 
 kamdbctl reinit
 
-sed -i -E  "s/mpath=/#mpath=/g" /etc/kamailio/default.cfg
-sed -i -E  's/modparam\("mi_fifo"/#modparam\("mi_fifo"/g' /etc/kamailio/default.cfg
+sed -i -E  "s/mpath=/#mpath=/g" /etc/kazoo/kamailio/default.cfg
+sed -i -E  's/modparam\("mi_fifo"/#modparam\("mi_fifo"/g' /etc/kazoo/kamailio/default.cfg
 
-cp /etc/kamailio/local.cfg /etc/kamailio/local.cfg.orig
+cp /etc/kazoo/kamailio/local.cfg /etc/kazoo/kamailio/local.cfg.orig
 
-rsync -av etc/kamailio/dbtext /etc/kamailio/
+rsync -av etc/kamailio/dbtext /etc/kazoo/kamailio/
 
 kamctl dispatcher add 1 sip:freeswitch.kazoo:11000 0 1 ' ' ' '
 
--- kamailio/build/config-local.sh
+++ kamailio/build/config-local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp /etc/kamailio/local.cfg.orig /etc/kamailio/local.cfg
-LOCAL=/etc/kamailio/local.cfg
+cp /etc/kazoo/kamailio/local.cfg.orig /etc/kazoo/kamailio/local.cfg
+LOCAL=/etc/kazoo/kamailio/local.cfg
 
 MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
 
