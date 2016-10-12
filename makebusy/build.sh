c8faa7697017d73f291863064a3d34ea737193d7
Roman Galeev <jamhed@2600hz.com>
2016-10-12 03:40:40 +0200

    makebusy update

--- makebusy/build.sh
+++ makebusy/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 FLAGS=${1:-""}
+REPO=${2:-"git@github.com:2600hz/make-busy.git"}
 cp -a ~/.ssh etc/
+echo Using repository $REPO
+../bin/get-commit $REPO > etc/commit
+echo Using repository $REPO commit: `cat etc/commit`
 docker build $FLAGS -t kazoo/makebusy .
