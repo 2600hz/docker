e8d211204cc62671aa2324005947aade6a181fdb
Roman Galeev <jamhed@2600hz.com>
2016-10-12 21:14:59 +0200

    separate system init and kazoo build

--- after-start.sh
+++ after-start.sh
@@ -4,12 +4,17 @@ echo Init the system
 cd kazoo
 ./sup crossbar_maintenance create_account admin kamailio.kazoo admin admin
 ./sup ecallmgr_maintenance add_fs_node freeswitch@freeswitch.kazoo
-./sup kazoo_media_maintenance import_prompts /home/user/kazoo-sounds/kazoo-core/en/us en-us
+
+git clone --depth 1 --no-single-branch https://github.com/2600hz/kazoo-sounds
+docker cp kazoo-sounds/kazoo-core/en/us kazoo.kazoo:/home/user
+./sup kazoo_media_maintenance import_prompts /home/user/us en-us
+docker exec -ti kazoo.kazoo rm -rf us
 
 docker cp monster-ui.kazoo:/usr/share/nginx/html/src/apps apps
 docker cp apps kazoo.kazoo:/home/user
 rm -rf apps
-
 ./sup crossbar_maintenance init_apps /home/user/apps http://kazoo.kazoo:8000/v2
+docker exec -ti kazoo.kazoo rm -rf apps
+
 
 docker exec -ti kamailio.kazoo kamctl dispatcher reload 
