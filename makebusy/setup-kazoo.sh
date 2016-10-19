a835f737b376d36c5cf580d5fd976d367193c18f
Roman Galeev <jamhed@2600hz.com>
2016-10-19 17:46:20 +0200

    makebusy fixes

--- makebusy/setup-kazoo.sh
+++ makebusy/setup-kazoo.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 ./sup kapps_config set token_buckets tokens_fill_rate 100
 ./sup kapps_config set token_buckets crossbar {}
+./sup kapps_config set callflow.park default_ringback_timeout 5000
+./sup kapps_config set conferences route_win_timeout 3000
+./sup crossbar_init start_mod cb_system_configs
 ./sup kz_datamgr flush_cache_docs
 docker commit couchdb.kazoo kazoo/couchdb-init
