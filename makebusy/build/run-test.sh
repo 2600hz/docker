3e034a370b076054e8b7f848fecbeea2c7c4f3e0
Roman Galeev <jamhed@2600hz.com>
2016-10-28 17:02:14 +0000

    better makebusy runner

--- makebusy/build/run-test.sh
+++ makebusy/build/run-test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /var/www/html/make-busy
 echo > /tmp/makebusy.log 
-echo > /tmp/makebusy.txt
-vendor/bin/phpunit --debug -v | tee /tmp/makebusy.txt
+vendor/bin/phpunit --debug -v $@
--- makebusy/run-test.sh
+++ makebusy/run-test.sh
@@ -2,8 +2,11 @@
 ./reset-kazoo.sh
 echo Waiting for kazoo to start
 watch -g "docker logs kazoo.kazoo | grep 'auto-started kapps'" > /dev/null
-docker exec -ti makebusy.kazoo ./run-test.sh
-docker cp makebusy.kazoo:/tmp/makebusy.log logs/
-docker cp makebusy.kazoo:/tmp/makebusy.txt logs/
-docker logs kazoo.kazoo > logs/kazoo.log
-
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\DeviceTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\ParkingTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\QuickCallTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\RingGroupTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\UserTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\VoicemailTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\ConferenceTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\WebhookTest:'
