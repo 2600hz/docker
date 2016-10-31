e358db297b7adb414a8eb2a4193c163980baf92c
Roman Galeev <jamhed@2600hz.com>
2016-10-31 17:49:56 +0000

    re-run failed tests

--- makebusy/run-test.sh
+++ makebusy/run-test.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+rm -f tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\DeviceTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\DeviceTest:' | tee -a tap.log
+./reset-kazoo.sh 
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\ParkingTest:' | tee -a tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\ParkingTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\QuickCallTest:' | tee -a tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\QuickCallTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\RingGroupTest:' | tee -a tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\RingGroupTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\UserTest:' | tee -a tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\UserTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\VoicemailTest:' | tee -a tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\VoicemailTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\ConferenceTest:' | tee -a tap.log
 ./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\ConferenceTest:'
-./reset-kazoo.sh
-docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\WebhookTest:'
+docker exec -ti makebusy.kazoo ./run-test.sh '--filter Callflow\\WebhookTest:' | tee -a tap.log
+tap/retap.pl tap.log
