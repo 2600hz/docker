17e9018ac4a080317d46a4db42f11a84f0e6fa8d
Roman Galeev <jamhed@2600hz.com>
2016-11-01 05:29:22 +0000

    common runner

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 FLAGS=${1:-"-td"}
+IMAGE=${2:-"kazoo/couchdb"}
 NETWORK=${NETWORK:-"kazoo"}
 NAME=couchdb.$NETWORK
-docker stop -t 1 $NAME
+docker stop $NAME
 docker rm $NAME
-docker run $FLAGS --net $NETWORK -h $NAME --name $NAME kazoo/couchdb
+docker run $FLAGS --net $NETWORK -h $NAME --name $NAME $IMAGE
