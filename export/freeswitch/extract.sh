6b738823f9cc5a88593b75207a96da37fe7f3282
Roman Galeev <jamhed@2600hz.com>
2016-11-28 17:50:25 +0100

    export with permissions as tar file

--- export/freeswitch/extract.sh
+++ export/freeswitch/extract.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 CONTAINER=${1:-"freeswitch.kazoo"}
 rm -rf freeswitch
+rm -f freeswitch.tar
 docker cp build/find-debian-deps.sh $CONTAINER:/root
 docker exec -ti --user root $CONTAINER /root/find-debian-deps.sh /usr/local/freeswitch | sed -e 's/\x0D//' > etc/deps
 docker exec -ti --user root $CONTAINER rm -f /root/find-debian-deps.sh
+curl http://files.freeswitch.org/repo/deb/debian/key.gpg > etc/freeswitch.key.gpg
 docker cp $CONTAINER:/usr/local/freeswitch freeswitch
 rm -rf freeswitch/sounds/*
-curl http://files.freeswitch.org/repo/deb/debian/key.gpg > etc/freeswitch.key.gpg
+tar cf freeswitch.tar freeswitch/ --owner=1000 --group=1000
+rm -rf freeswitch
--- export/kamailio/extract.sh
+++ export/kamailio/extract.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 CONTAINER=${1:-"kamailio.kazoo"}
 rm -rf kamailio
-mkdir kamailio
+rm -rf kamailio.tar
+mkdir -p kamailio/usr/local
+mkdir -p kamailio/etc
+mkdir -p kamailio/root
 docker cp build/find-debian-deps.sh $CONTAINER:/root
 docker exec -ti --user root $CONTAINER /root/find-debian-deps.sh /usr/local | sed -e 's/\x0D//' > etc/deps
 docker exec -ti --user root $CONTAINER rm -f /root/find-debian-deps.sh
-docker cp $CONTAINER:/usr/local kamailio/bin
-docker cp $CONTAINER:/etc/kamailio kamailio/etc
-docker cp $CONTAINER:/root/run.sh kamailio/run.sh
-docker cp $CONTAINER:/root/config-local.sh kamailio/config-local.sh
+docker cp $CONTAINER:/usr/local kamailio/usr/
+docker cp $CONTAINER:/etc/kamailio kamailio/etc/kamailio
+docker cp $CONTAINER:/root/run.sh kamailio/root/run.sh
+docker cp $CONTAINER:/root/config-local.sh kamailio/root/config-local.sh
+cd kamailio && tar cf ../kamailio.tar ./ --owner=0 --group=0 && cd ../
+rm -rf kamailio
