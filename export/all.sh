5f8ce906e51278dce7b6a034d3c2786d7b0e4c04
Roman Galeev <jamhed@2600hz.com>
2016-11-29 15:54:59 +0100

    add monster-ui to default export

--- export/all.sh
+++ export/all.sh
@@ -13,8 +13,13 @@ cd ../kamailio
 cd ../freeswitch
 ./extract.sh freeswitch.$NETWORK
 ./build.sh
+cd ../monster-ui
+./extract.sh monster-ui.$NETWORK
+./build.sh
+cd ../
 docker push 2600hz/couchdb
 docker push 2600hz/rabbitmq
 docker push 2600hz/kazoo
 docker push 2600hz/kamailio
 docker push 2600hz/freeswitch
+docker push 2600hz/monster-ui
