c6f5e912ac459828a895652b0955265078c98147
Roman Galeev <jamhed@2600hz.com>
2016-11-29 12:32:03 +0100

    forgotten export

--- kazoo/build/run.sh
+++ kazoo/build/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-KAZOO_CONFIG=$HOME/config.ini
+export KAZOO_CONFIG=$HOME/config.ini
 sed -i "s|couchdb.kazoo|$COUCHDB|" $KAZOO_CONFIG
 sed -i "s|rabbitmq.kazoo|$RABBITMQ|" $KAZOO_CONFIG
-KAZOO_NODE=$NODE_NAME@$(hostname)
-KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr
+export KAZOO_NODE=$NODE_NAME@$(hostname)
+export KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr
 export RELX_REPLACE_OS_VARS=true
 export KZname="-name $KAZOO_NODE"
 
