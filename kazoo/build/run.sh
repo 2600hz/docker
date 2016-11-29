c14d29e00aebb1a9a7ff15e2e32a17cda70b5c95
Roman Galeev <jamhed@2600hz.com>
2016-11-29 11:54:08 +0100

    allow to specify rabbitmq and couchdb

--- kazoo/build/run.sh
+++ kazoo/build/run.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
-. erlang/activate
-
-if [ "$KAZOO_CONFIG" = "" ]
-then
-   export KAZOO_CONFIG=$HOME/config.ini
-fi
-
-if [ "$KAZOO_NODE" = "" ]
-then
-   KAZOO_NODE=kazoo@$(hostname)
-fi
-
-CMD=$1
-if [ "$CMD" = "" ]
-then
-   export KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr
-   CMD=console
-else
-   shift
-fi
-
+KAZOO_CONFIG=$HOME/config.ini
+sed -i "s|couchdb.kazoo|$COUCHDB|" $KAZOO_CONFIG
+sed -i "s|rabbitmq.kazoo|$RABBITMQ|" $KAZOO_CONFIG
+KAZOO_NODE=$NODE_NAME@$(hostname)
+KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr
 export RELX_REPLACE_OS_VARS=true
 export KZname="-name $KAZOO_NODE"
 
 cd kazoo
-exec _rel/kazoo/bin/kazoo $CMD "$*"
+exec _rel/kazoo/bin/kazoo console
--- kazoo/run.sh
+++ kazoo/run.sh
@@ -4,4 +4,13 @@ NETWORK=${NETWORK:-"kazoo"}
 NAME=kazoo.$NETWORK
 docker stop -t 1 $NAME
 docker rm -f $NAME
-docker run $FLAGS --net $NETWORK -h $NAME --name $NAME kazoo/kazoo
+docker run $FLAGS \
+	--net $NETWORK \
+	-h $NAME \
+	--name $NAME \
+	--env NETWORK=kazoo \
+	--env COUCHDB=couchdb.$NETWORK \
+	--env RABBITMQ=rabbitmq.$NETWORK \
+	--env NODE_NAME=kazoo \
+	--env KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr \
+	kazoo/kazoo
