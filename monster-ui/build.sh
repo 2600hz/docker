c3dfe3f90c6f96e62f8ac190faecab0791e3655d
Roman Galeev <jamhed@2600hz.com>
2016-11-29 15:09:27 +0100

    remove keys before build

--- monster-ui/build.sh
+++ monster-ui/build.sh
@@ -3,6 +3,7 @@ NETWORK=${NETWORK:-"kazoo"}
 BUILD_ARG=${1:-""}
 REPO=${2:-"https://github.com/2600hz/monster-ui.git"}
 COMMIT=${3:-"$(../bin/get-commit $REPO)"}
+rm -rf etc/.ssh
 if [ -d ~/.ssh ]
 then
 	cp -a ~/.ssh etc/
