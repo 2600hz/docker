3622d1afe13baa3ec190fd6e04a3183f37c2f0ed
Roman Galeev <jamhed@2600hz.com>
2016-11-29 13:03:08 +0100

    stop daily before rebuild

--- pipeline/daily.sh
+++ pipeline/daily.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# stop daily segment
+./stop.sh daily
+
 # save some space
 docker rmi $(docker images -a --filter dangling=true -q)
 docker rm $(docker ps -a --filter status=exited -q)
