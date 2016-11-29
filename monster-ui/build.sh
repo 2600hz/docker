015ce5e002e1053228364ceaaf66f62afbef915c
Roman Galeev <jamhed@2600hz.com>
2016-11-29 15:03:19 +0100

    fix: syntax error

--- monster-ui/build.sh
+++ monster-ui/build.sh
@@ -3,7 +3,10 @@ NETWORK=${NETWORK:-"kazoo"}
 BUILD_ARG=${1:-""}
 REPO=${2:-"https://github.com/2600hz/monster-ui.git"}
 COMMIT=${3:-"$(../bin/get-commit $REPO)"}
-if [ -d ~/.ssh ] && cp -a ~/.ssh etc/
+if [ -d ~/.ssh ]
+then
+	cp -a ~/.ssh etc/
+fi
 echo Using repository $REPO commit $COMMIT
 echo $COMMIT > etc/commit
 docker build $BUILD_ARG -t $NETWORK/monster-ui --build-arg REPO=$REPO .
