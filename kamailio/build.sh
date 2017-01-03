2cd668bf77700f04e9b12d8174874db4e40a758f
Roman Galeev <jamhed@2600hz.com>
2017-01-03 18:18:18 +0100

    fix kamailio version to 4.4.4

--- kamailio/build.sh
+++ kamailio/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 NETWORK=${NETWORK:-"kazoo"}
-COMMIT=${1:-"HEAD"}
+COMMIT=${1:-"2250b3e562d6dbcab1477c7ed5a71daa7e462bb9"} # fix version to 4.4.4
 docker build --build-arg=COMMIT=$COMMIT -t $NETWORK/kamailio .
