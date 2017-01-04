7d46d91d8fe77f73b763f90a756bd6eabef9dd1d
karl anderson <karl@2600hz.com>
2017-01-04 21:10:42 +0000

    allow the name to be set

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FLAGS=${1:-"-td"}
 NETWORK=${NETWORK:-"kazoo"} 
-NAME=rabbitmq.$NETWORK
+NAME=${NAME:-"rabbitmq.$NETWORK"}
 docker stop -t 1 $NAME
 docker rm -f $NAME
 docker run $FLAGS --net $NETWORK -h $NAME --name $NAME kazoo/rabbitmq
