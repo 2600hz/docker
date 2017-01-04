507819dc9b1a9145f73e341e8bb0f02ee025275f
karl anderson <karl@2600hz.com>
2017-01-04 15:47:51 +0000

    support naming the node and consitancy with other run scripts

--- couchdb2.0/run.sh
+++ couchdb2.0/run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 FLAGS=${1:-"-td"}
 NETWORK=${NETWORK:-"kazoo"}
-NAME=couchdb2.$NETWORK
+NAME=${NAME:-"couchdb2.$NETWORK"}
 docker stop $NAME
 docker rm $NAME
-docker run $FLAGS --net $NETWORK -h $NAME --name $NAME kazoo/couchdb2
+docker run $FLAGS \
+  --net $NETWORK \
+  -h $NAME \
+  --name $NAME \
+  --env NODENAME=$NAME \
+  kazoo/couchdb2
