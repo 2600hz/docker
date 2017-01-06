9fa3d6de7cf4f97396b2daf2da40121e6e87c97d
Roman Galeev <jamhed@2600hz.com>
2017-01-06 14:39:51 +0000

    ci: reliable export

--- export/all.sh
+++ export/all.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 export NETWORK=${NETWORK:-"kazoo"}
 docker rmi -f $(docker images | grep "2600hz/" | cut -d' ' -f 1)
-cd export/couchdb
-./build.sh
-cd ../rabbitmq
-./build.sh
-cd ../kazoo
-./extract.sh kazoo.$NETWORK
-./build.sh
-cd ../kamailio
-./extract.sh kamailio.$NETWORK
-./build.sh
-cd ../freeswitch
-./extract.sh freeswitch.$NETWORK
-./build.sh
-cd ../monster-ui
-./extract.sh monster-ui.$NETWORK
-./build.sh
+cd ~/kazoo-docker/export/couchdb && ./build.sh
+cd ../rabbitmq && ./build.sh
+cd ../kazoo && ./extract.sh kazoo.$NETWORK && ./build.sh
+cd ../kamailio && ./extract.sh kamailio.$NETWORK && ./build.sh
+cd ../freeswitch && ./extract.sh freeswitch.$NETWORK && ./build.sh
+cd ../monster-ui && ./extract.sh monster-ui.$NETWORK && ./build.sh
 cd ../
 docker push 2600hz/couchdb
 docker push 2600hz/rabbitmq
