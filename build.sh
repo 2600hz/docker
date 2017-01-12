5a8dfaab80e7b75759679f8a2ab414484a6fd4ce
Roman Galeev <jamhed@2600hz.com>
2017-01-12 20:49:54 +0100

    shorter build

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
-cd base-os
-./build.sh
-cd ../kazoo-erlang
-./build.sh
-cd ../rabbitmq
-./build.sh
-cd ../couchdb
-./build.sh
-# fix kamailio version to 4.4.4
-cd ../kamailio
-./build.sh 2250b3e562d6dbcab1477c7ed5a71daa7e462bb9
-cd ../freeswitch
-./build.sh
-cd ../kazoo
-./build.sh
-cd ../monster-ui
-./build.sh
+cd base-os && ./build.sh
+cd ../kazoo-erlang && ./build.sh
+cd ../rabbitmq && ./build.sh
+cd ../couchdb &&./build.sh
+cd ../kamailio && ./build.sh
+cd ../freeswitch && ./build.sh
+cd ../kazoo && ./build.sh
+cd ../monster-ui && ./build.sh
