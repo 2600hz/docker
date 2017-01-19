2dff647a22fb396a161ae92eb68269491455adc0
Roman Galeev <jamhed@2600hz.com>
2017-01-19 17:25:15 +0100

    build: check minimal docker version

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+INSTALLED=`docker version --format '{{.Server.Version}}'`
+REQUIRED="1.9.0"
+if [ "$(printf "$REQUIRED\n$INSTALLED" | sort -V | head -n1)" == "$INSTALLED" ] && [ "$INSTALLED" != "$REQUIRED" ]
+then
+	echo Docker version $INSTALLED is probably too old, required version is $REQUIRED
+	exit
+fi
 cd base-os && ./build.sh
 cd ../kazoo-erlang && ./build.sh
 cd ../rabbitmq && ./build.sh
