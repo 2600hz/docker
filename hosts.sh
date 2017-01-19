d4d18505ccc5965556fde3bcf04510a15100a975
Roman Galeev <jamhed@2600hz.com>
2017-01-19 17:02:48 +0100

    hosts.sh: skip non-running images

--- hosts.sh
+++ hosts.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 NETWORK=${1:-"kazoo"}
-echo `bin/get-ip monster-ui.$NETWORK` monster-ui.$NETWORK
-echo `bin/get-ip kazoo.$NETWORK` kazoo.$NETWORK
-echo `bin/get-ip rabbitmq.$NETWORK` rabbitmq.$NETWORK 
-echo `bin/get-ip freeswitch.$NETWORK` freeswitch.$NETWORK
-echo `bin/get-ip kamailio.$NETWORK` kamailio.$NETWORK
-echo `bin/get-ip couchdb.$NETWORK` couchdb.$NETWORK
-echo `bin/get-ip makebusy.$NETWORK` makebusy.$NETWORK
-echo `bin/get-ip makebusy-fs-auth.$NETWORK` makebusy-fs-auth.$NETWORK
-echo `bin/get-ip makebusy-fs-pbx.$NETWORK` makebusy-fs-pbx.$NETWORK
-echo `bin/get-ip makebusy-fs-carrier.$NETWORK` makebusy-fs-carrier.$NETWORK
-
-
-
+for CONTAINER in monster-ui kazoo rabbitmq freeswitch couchdb makebusy makebusy-fs-auth makebusy-fs-pbx makebusy-fs-carrier
+do
+	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.$NETWORK)" = "true" ]
+	then
+		echo `bin/get-ip $CONTAINER.$NETWORK` $CONTAINER.$NETWORK
+	fi
+done
+exit
