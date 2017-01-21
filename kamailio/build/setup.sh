30d6b30bf8baf8a48cdedb4f685e39afee0369d8
Roman Galeev <jamhed@2600hz.com>
2017-01-21 03:17:42 +0100

    kamailio: get full tree (to choose commits)

--- kamailio/build/setup.sh
+++ kamailio/build/setup.sh
@@ -2,7 +2,7 @@
 COMMIT=$(cat /root/commit)
 
 git clone https://github.com/2600hz/kazoo-configs.git kazoo-configs
-git clone --depth 1 --no-single-branch https://github.com/kamailio/kamailio.git kamailio
+git clone https://github.com/kamailio/kamailio.git kamailio
 
 cd kazoo-configs
 git checkout ef98bf453cb81475bbf5ccbc0635749a975b093c
