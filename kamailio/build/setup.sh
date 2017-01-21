0d4e981263097edb34a39a310101c4b0319b4e3c
Roman Galeev <jamhed@2600hz.com>
2017-01-21 03:11:10 +0100

    kamailio: fix configs

--- kamailio/build/setup.sh
+++ kamailio/build/setup.sh
@@ -4,5 +4,9 @@ COMMIT=$(cat /root/commit)
 git clone --depth 1 --no-single-branch https://github.com/2600hz/kazoo-configs.git kazoo-configs
 git clone --depth 1 --no-single-branch https://github.com/kamailio/kamailio.git kamailio
 
+cd kazoo-configs
+git checkout ef98bf453cb81475bbf5ccbc0635749a975b093c
+cd ../
+
 cd kamailio
 git checkout $COMMIT
