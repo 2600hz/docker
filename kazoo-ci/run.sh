6048dd9969136ed8364d42dd94bc0462c59f008c
Roman Galeev <jamhed@2600hz.com>
2017-01-26 04:35:25 +0100

    ci: run always

--- kazoo-ci/run.sh
+++ kazoo-ci/run.sh
@@ -19,6 +19,7 @@ docker run -v kazoo-ci:/home/user/volume --user root --entrypoint ./volume.sh ka
 
 echo -n "starting: $NAME "
 docker run \
+	 --restart unless-stopped \
 	-h $NAME \
 	--name $NAME \
 	-v /var/run/docker.sock:/var/run/docker.sock \
