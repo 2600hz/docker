4f423962d578d34763b5d104507630e2af078c18
Roman Galeev <jamhed@2600hz.com>
2017-01-30 04:33:09 +0100

    kazoo-ci: proper checkout

--- kazoo-ci/build/run.sh
+++ kazoo-ci/build/run.sh
@@ -5,5 +5,4 @@ mkdir -p /run/apache2
 sed -i 's|^DocumentRoot.*|DocumentRoot /home/user/make-busy/ci|' /etc/apache2/httpd.conf
 sed -i 's|^User.*|User user|' /etc/apache2/httpd.conf
 sed -i "s|^#ServerName.*|ServerName $IP|" /etc/apache2/httpd.conf
-sed -i 's|<Directory "/var/www/localhost/htdocs">|<Directory "/home/user/make-busy/ci">|' /etc/apache2/httpd.conf
 exec /usr/sbin/httpd -DFOREGROUND
