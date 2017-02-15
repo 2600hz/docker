50900ed0156e326f19bab130d1ce82f79bf827ae
Roman Galeev <jamhed@2600hz.com>
2017-02-15 19:34:17 +0100

    bump erlang version to 19.2

--- kazoo-erlang/build/setup-erlang.sh
+++ kazoo-erlang/build/setup-erlang.sh
@@ -2,7 +2,7 @@
 curl -O -k https://raw.githubusercontent.com/kerl/kerl/master/kerl
 chmod +x kerl
 ./kerl update releases
-./kerl build 18.3 18.3
-./kerl install 18.3 erlang
+./kerl build 19.2 19.2
+./kerl install 19.2 erlang
 . erlang/activate
 ./kerl cleanup all
