2feca0b5cdb5ef9b1a3afb7dfd6602ae69e9e677
Roman Galeev <jamhed@2600hz.com>
2017-02-16 17:26:55 +0100

    monster-ui: complete external uri to configure

--- export/monster-ui/build/run.sh
+++ export/monster-ui/build/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CFG=/usr/share/nginx/html/js/config.js
-sed -i "s|kazoo.kazoo|$KAZOO|" $CFG
+sed -i "s|http://kazoo.deploy:8000/v2/|$KAZOO_URL|" $CFG
 exec /usr/sbin/nginx -g "daemon off;"
