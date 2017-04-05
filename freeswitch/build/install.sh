372ced5c2a3a23b1cf1d5e059d37dc998732a947
Roman Galeev <jamhed@2600hz.com>
2017-04-05 21:37:46 +0200

    Add sound files to default fs setup
    It turns out these files are required for some
    functionality (parking, namely).

--- freeswitch/build/install.sh
+++ freeswitch/build/install.sh
@@ -6,9 +6,13 @@ groupadd freeswitch
 
 adduser --disabled-password  --quiet --system --home /usr/local/freeswitch --ingroup freeswitch freeswitch
 
+git clone --depth 1 --no-single-branch https://github.com/2600hz/kazoo-sounds
+cp -a kazoo-sounds/freeswitch /usr/local/freeswitch/sounds
+
 chown -R freeswitch:freeswitch /usr/local/freeswitch/
 chmod -R ug=rwX,o= /usr/local/freeswitch/
 chmod -R u=rwx,g=rx /usr/local/freeswitch/bin/*
 
 rm -rf /usr/local/freeswitch/conf
 cp -a /home/user/kazoo-configs/freeswitch /usr/local/freeswitch/conf/
+
