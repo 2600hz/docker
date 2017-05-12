6217a56b29f3c363970e7157458089d0e93bc6e9
Tor Erik Linnerud <tel@jklm.no>
2017-05-12 15:48:34 -0400

    More robust configuration search and replace
    Why this is needed:
    The dockerhub images are created by extracting files from a locally running docker instance.
    On the local instance, config-local.sh has already executed and replaced kamailio.2600hz.com with kamailio.kazoo.
    When config-local.sh then runs on the image uploaded to dockerhub, no replacement takes place as it is looking for
    kamailio.2600hz.com and the file contains kamailio.kazoo.

--- kamailio4.0/build/config-local.sh
+++ kamailio4.0/build/config-local.sh
@@ -3,12 +3,13 @@ cp /etc/kamailio/local.cfg.orig /etc/kamailio/local.cfg
 LOCAL=/etc/kamailio/local.cfg
 
 MY_IP=$(hostname -i)
+MY_HOSTNAME=$(hostname)
 
 # ip address
 /bin/sed -i "s/MY_IP_ADDRESS!127.0.0.1/MY_IP_ADDRESS!$MY_IP/g" $LOCAL
 
-# domain
-/bin/sed -i "s/kamailio.2600hz.com/$HOSTNAME/g" $LOCAL
+# hostname
+/bin/sed -i "s/MY_HOSTNAME!.*!/MY_HOSTNAME!$MY_HOSTNAME!/g" $LOCAL
 
 # rabbitmq
 /bin/sed -i "s/MY_AMQP_URL!kazoo:\/\/guest:guest@127.0.0.1:5672/MY_AMQP_URL!kazoo:\/\/guest:guest@$RABBITMQ:5672/g" $LOCAL
